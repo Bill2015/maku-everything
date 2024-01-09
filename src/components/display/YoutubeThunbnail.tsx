83de91b356524e24518995ead5f36fc99cc912b8
Bill2015 <Bill317016@gmail.com>
2024-01-09 23:55:19 +0800

    Fix: fix youtube thumbnail not display properly
    最快的做法的直接將把 `maxresdefault.jpg` 改成 `0.jpg`
    但是這樣的壞處就是如果影片有更高解析度的 Thumbnail 將會讀不到
    
    所以採用第二個方法
    判斷是否有 `maxresdefault.jpg` 這個圖片
    不過 https://img.youtube.com/vi/eQTtvI2ZGKA/0.jpg 這個網址有 CORS 的限制
    所以無法直接透過 Request 的方式判斷圖片是否有效
    
    最後的解決方法如下:
    假如圖片無法讀去，這個網頁(https://img.youtube.com)會丟一個預設 `120x90` 的錯誤圖片來顯示
    那麼我就透過這個 `120x90` 的大小來判斷是否無法讀取到 `maxresdefault.jpg`
    如果讀出的大小都大於 `120x90` 就代表有 `maxresdefault.jpg` 這個圖片
    如果小於 `130x90` 則使用 `0.jpg`
    
    - Related Issues
        - #8

--- src/components/display/YoutubeThunbnail.tsx
+++ src/components/display/YoutubeThunbnail.tsx
@@ -1,28 +1,73 @@
 import { CiImageOff } from 'react-icons/ci';
-import { Center } from '@mantine/core';
+import { Center, Image } from '@mantine/core';
 import { getYoutubeVideoId } from '@utils/urlParser';
 
-import { ResponsiveImage, ResponsiveImageProps } from './ResponsiveImage';
+import { useMemo, useRef, useState } from 'react';
+import { ResponsiveImageProps } from './ResponsiveImage';
 
 export interface YoutubeThumbnailProps extends Omit<ResponsiveImageProps, 'src'> {
     url: string;
 }
 
+/**
+ * Display Youtube Video Thumbnail \
+ * Normally, It will display **Max Resolution** Image, if not it will get **second high resolution** image to display \
+ *
+ * #### Max Resolution
+ * https://img.youtube.com/vi/${videoId}/maxresdefault.jpg
+ *
+ * #### Default
+ * https://img.youtube.com/vi/${videoId}/0.jpg */
 export function YoutubeThumbnail(props: YoutubeThumbnailProps) {
     const { url } = props;
 
-    const videoId = getYoutubeVideoId(url);
+    const [isLoaded, setIsLoaded] = useState<boolean>(false);
+    const ref = useRef<HTMLImageElement | null>(null);
 
-    if (!videoId) {
+    function createURL(videoId: string, type: 'default' | 'max-resoultion') {
+        return `https://img.youtube.com/vi/${videoId}/${(type === 'default') ? '0' : 'maxresdefault'}.jpg`;
+    }
+
+    const imageURL = useMemo(() => {
+        const videoId = getYoutubeVideoId(url);
+        if (!videoId) {
+            return '';
+        }
+
+        if (!ref || !ref.current || !isLoaded) {
+            return createURL(videoId, 'max-resoultion');
+        }
+        const w = ref.current!.naturalWidth;
+        const h = ref.current!.naturalHeight;
+
+        // Because the youtube image will return 120x90 default image if no resource found.
+        // Therefore we can use size to evaluate dose image have max resoultion version or not.
+        // NOTE: Because CORS, so we can't use request to determine.
+        if (w <= 130 && h <= 100) {
+            return createURL(videoId, 'default');
+        }
+
+        return createURL(videoId, 'max-resoultion');
+    }, [url, isLoaded]);
+
+    if (!imageURL) {
         return (
             <Center>
                 <CiImageOff style={{ width: '50%', height: '50%' }} />
             </Center>
         );
     }
+
     return (
-        <ResponsiveImage
-            src={`https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`}
+        <Image
+            ref={ref}
+            style={{
+                objectFit: 'scale-down',
+                width:     '100%',
+                height:    '100%',
+            }}
+            onLoad={() => setIsLoaded(true)}
+            src={imageURL}
         />
     );
 }
