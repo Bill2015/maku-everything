552dc9999e6b5646861b0de31f3737cd5615024d
Bill2015 <Bill317016@gmail.com>
2024-02-06 03:04:21 +0800

    Fix: change resource add page folder name

--- src/router/RoutingTable.tsx
+++ src/router/RoutingTable.tsx
@@ -5,7 +5,7 @@ import { CategoryContainer } from '@pages/common';
 const CategoryListPage = React.lazy(() => import(/* webpackChunkName: "category-page" */ '@pages/category-list/CategoryListPage'));
 const ResourceListPage = React.lazy(() => import(/* webpackChunkName: "resources-page" */ '@pages/resource-list/ResourceListPage'));
 const ResourcesDetailPage = React.lazy(() => import(/* webpackChunkName: "resources-detail-page" */ '@pages/resource-detail/ResourceDetailPage'));
-const ResourceAddPage = React.lazy(() => import(/* webpackChunkName: "resources-import-page" */ '@pages/resource-import/ResourceAddPage'));
+const ResourceAddPage = React.lazy(() => import(/* webpackChunkName: "resources-add-page" */ '@pages/resource-add/ResourceAddPage'));
 
 export const ROUTE_OBJECTS: RouteObject[] = [
     {
